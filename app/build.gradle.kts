d91dc81b9ee9438b1c321e60539e43dcbd59dbd5
Aditya Gyawali <csaditya16@gmail.com>
2026-03-11 12:42:33 -0600

    Update build.gradle.kts added implemetation for scanner

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,5 +63,6 @@ dependencies {
     implementation("com.google.firebase:firebase-firestore")
     implementation("com.google.firebase:firebase-storage")
     implementation("com.github.bumptech.glide:glide:4.16.0")
+    implementation("com.journeyapps:zxing-android-embedded:4.3.0")
     annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
 }
