d975620e3a7562dea795d758817169f3b9cc5e76
Arnav Chandra <chandra3@ualberta.ca>
2026-04-06 11:06:06 -0600

    fixed intent tests

---
+++ app/src/androidTest/java/com/example/myapplication/AdminBrowseEventsTest.java
@@ -0,0 +1,114 @@
+package com.example.myapplication;
+
+import android.view.View;
+
+import androidx.test.espresso.UiController;
+import androidx.test.espresso.ViewAction;
+import androidx.test.espresso.contrib.RecyclerViewActions;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+
+import org.hamcrest.Matcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.*;
+
+/**
+ * Intent tests for AdminBrowseEventsActivity.
+ * Verifies browsing, navigation, and interaction with event list.
+ */
+@RunWith(AndroidJUnit4.class)
+public class AdminBrowseEventsTest {
+
+    @Rule
+    public ActivityTestRule<AdminBrowseEventsActivity> rule =
+            new ActivityTestRule<>(AdminBrowseEventsActivity.class);
+
+    /**
+     * Helper method to pause execution to allow async Firebase data to load.
+     */
+    private void waitFor(long millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Test that the RecyclerView displaying events is visible.
+     */
+    @Test
+    public void testRecyclerViewDisplayed() {
+        onView(withId(R.id.recyclerViewAdminEvents))
+                .check(matches(isDisplayed()));
+    }
+
+    /**
+     * Test clicking on an event item in the RecyclerView.
+     * This should trigger the event detail dialog.
+     */
+    @Test
+    public void testClickEventItem() {
+
+        waitFor(2000); // wait for Firestore data
+
+        onView(withId(R.id.recyclerViewAdminEvents))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+    }
+
+    /**
+     * Test clicking the comments button inside a RecyclerView item.
+     * This should launch the CommentActivity.
+     */
+    @Test
+    public void testClickCommentsButton() {
+
+        waitFor(2000); // wait for Firestore data
+
+        onView(withId(R.id.recyclerViewAdminEvents))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(
+                        0,
+                        clickChildViewWithId(R.id.btn_view_comments)
+                ));
+    }
+
+    /**
+     * Test navigation to Admin Logs screen using bottom navigation.
+     */
+    @Test
+    public void testNavigateToLogs() {
+        onView(withId(R.id.nav_admin_logs))
+                .perform(click());
+    }
+
+    /**
+     * Helper method to click a child view inside a RecyclerView item.
+     */
+    public static ViewAction clickChildViewWithId(int id) {
+        return new ViewAction() {
+            @Override
+            public Matcher<View> getConstraints() {
+                return isDisplayed();
+            }
+
+            @Override
+            public String getDescription() {
+                return "Click on child view with id " + id;
+            }
+
+            @Override
+            public void perform(UiController uiController, View view) {
+                View v = view.findViewById(id);
+                if (v != null) {
+                    v.performClick();
+                }
+            }
+        };
+    }
+}
---
+++ app/src/androidTest/java/com/example/myapplication/AdminBrowseProfilesTest.java
@@ -0,0 +1,91 @@
+package com.example.myapplication;
+
+import android.view.View;
+
+import androidx.test.espresso.UiController;
+import androidx.test.espresso.ViewAction;
+import androidx.test.espresso.contrib.RecyclerViewActions;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+
+import org.hamcrest.Matcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.*;
+
+/**
+ * Intent tests for AdminBrowseProfilesActivity.
+ * Verifies browsing, navigation, and interaction with profile list.
+ */
+@RunWith(AndroidJUnit4.class)
+public class AdminBrowseProfilesTest {
+
+    @Rule
+    public ActivityTestRule<AdminBrowseProfilesActivity> rule =
+            new ActivityTestRule<>(AdminBrowseProfilesActivity.class);
+
+    /**
+     * Helper method to wait for Firebase data to load.
+     */
+    private void waitFor(long millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Test that RecyclerView displaying profiles is visible.
+     */
+    @Test
+    public void testRecyclerViewDisplayed() {
+        onView(withId(R.id.recyclerViewAdminProfiles))
+                .check(matches(isDisplayed()));
+    }
+
+    /**
+     * Test clicking on a profile item.
+     * This should open the profile detail dialog.
+     */
+    @Test
+    public void testClickProfileItem() {
+
+        waitFor(2000); // wait for Firestore
+
+        onView(withId(R.id.recyclerViewAdminProfiles))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+    }
+
+    /**
+     * Test navigation to Admin Events screen.
+     */
+    @Test
+    public void testNavigateToEvents() {
+        onView(withId(R.id.nav_admin_events))
+                .perform(click());
+    }
+
+    /**
+     * Test navigation to Admin Logs screen.
+     */
+    @Test
+    public void testNavigateToLogs() {
+        onView(withId(R.id.nav_admin_logs))
+                .perform(click());
+    }
+
+    /**
+     * Test navigation to Admin Images screen.
+     */
+    @Test
+    public void testNavigateToImages() {
+        onView(withId(R.id.nav_admin_images))
+                .perform(click());
+    }
+}
