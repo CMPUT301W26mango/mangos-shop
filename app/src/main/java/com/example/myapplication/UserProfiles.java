707b0e932da691ffe4b9c40cd4c804ed68972118
Arnav Chandra <chandra3@ualberta.ca>
2026-04-06 15:55:57 -0600

    added more javadocs

--- app/src/main/java/com/example/myapplication/UserProfiles.java
+++ app/src/main/java/com/example/myapplication/UserProfiles.java
@@ -3,9 +3,8 @@ package com.example.myapplication;
 import com.google.firebase.firestore.DocumentId;
 
 /**
- * User information and roles description
- * This is what takes in the user info for the profiles to be created.
- * Also the info for admin requests
+ * Model class representing a user profile in the application.
+ * Stores user information and role details for account management and admin requests.
  */
 
 public class UserProfiles {
@@ -20,7 +19,9 @@ public class UserProfiles {
     @DocumentId
     private String deviceId;
 
-    // needed by Firestore (for data mapping)
+    /**
+     * Empty constructor required for Firebase or default object creation.
+     */
     public UserProfiles() {}
 
     /**
@@ -42,51 +43,116 @@ public class UserProfiles {
         this.adminRequested = false;
         this.deviceId = deviceId;
     }
-
+    /**
+     * Returns the device ID.
+     *
+     * @return device ID
+     */
     public String getDeviceId() {
         return deviceId;
     }
-
+    /**
+     * Sets the device ID.
+     *
+     * @param deviceId device ID
+     */
     public void setDeviceId(String deviceId) {
         this.deviceId = deviceId;
     }
-
+    /**
+     * Returns the user's name.
+     *
+     * @return user name
+     */
     public String getName() {
         return name;
     }
+    /**
+     * Sets the user's name.
+     *
+     * @param name user name
+     */
     public void setName(String name) {
         this.name = name;
     }
+    /**
+     * Returns the user's email.
+     *
+     * @return user email
+     */
     public String getEmail() {
         return email;
     }
+    /**
+     * Sets the user's email.
+     *
+     * @param email user email
+     */
     public void setEmail(String email) {
         this.email = email;
     }
+    /**
+     * Returns the user's phone number.
+     *
+     * @return phone number
+     */
     public String getPhone() {
         return phone;
     }
+    /**
+     * Sets the user's phone number.
+     *
+     * @param phone phone number
+     */
     public void setPhone(String phone) {
         this.phone = phone;
     }
+    /**
+     * Returns the user's role.
+     *
+     * @return user role
+     */
     public String getRole() {
         return role;
     }
+    /**
+     * Sets the user's role.
+     *
+     * @param role user role
+     */
     public void setRole(String role) {
         this.role = role;
     }
 
-    //TA idea
+    /**
+     * Returns whether the user is an admin.
+     *
+     * @return true if admin, false otherwise
+     */
     public boolean getIsAdmin() {
         return isAdmin;
     }
+    /**
+     * Sets whether the user is an admin.
+     *
+     * @param isAdmin admin status
+     */
     public void setIsAdmin(boolean isAdmin) {
         this.isAdmin = isAdmin;
     }
-
+    /**
+     * Returns whether the user has requested admin privileges.
+     *
+     * @return true if admin requested, false otherwise
+     */
     public boolean getAdminRequested() {
         return adminRequested;
     }
+    /**
+     * Sets whether the user has requested admin privileges.
+     *
+     * @param adminRequested admin request status
+     */
     public void setAdminRequested(boolean adminRequested) {
         this.adminRequested = adminRequested;
     }
